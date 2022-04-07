926ce8575dfa52a4d8ef27dd460b850c2281d819
Ian Molina <ianmgdaw@gmail.com>
2022-04-07 16:59:01 +0200

    Subidas clases

---
+++ GEEyEv2/src/main/java/GEEyEv2/Controlador.java
@@ -0,0 +1,18 @@
+package GEEyEv2;
+
+import java.sql.*;
+/*
+ * Proyecto GEEyE_V2 - Archivo Controlador.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/**
+ *
+ * @author Cristina Carrascosa <cctausias@gmail.com>, Ian Molina , Rai Vilar
+ * @version 1.0
+ * @date 6 abr. 2022 18:47:08
+ */
+public class Controlador {
+
+}
